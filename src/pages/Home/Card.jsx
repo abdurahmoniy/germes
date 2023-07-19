298f4fc12e375320b9863ccb3a0ed12e3742473b
abdurahmoniy <azizbekabdurahmoniy@gmail.com>
2023-07-19 18:32:18 +0500

    updated products

--- src/pages/Home/Card.jsx
+++ src/pages/Home/Card.jsx
@@ -1,9 +1,60 @@
-import React from 'react'
+import React from "react";
+import star from "../../assets/Icons/Star/Main.png";
+import cart from "../../assets/Icons/Cart/Main2.png";
 
-export default function Card() {
+const Card = ({ prod }) => {
+  console.log(prod);
   return (
-    <div>
-      
+    <div className="flex justify-center py-8">
+      <div className="grid grid-cols-5 gap-x-14 gap-y-8">
+        {prod.map((item) => (
+          <div className="border w-[240px] rounded ">
+            <div className="py-2 flex justify-between items-center">
+              <img src={item.cap} alt="" className="w-[130px]" />
+              <img src={star} alt="" className="mx-2" />
+            </div>
+            <div className="text-center flex justify-center">
+              <img src={item.img} alt="" />
+            </div>
+            <div className="w-full px-2 my-3">
+              <div className="text-[14px] font-semibold">
+                {item.title}
+              </div>
+              <div className="text-[12px] py-1">
+                {item.desc1} <br />
+                {item.desc2}
+              </div>
+              <div className="text-[10px] flex underline">
+                <a href="#">бесплатная доставка</a>
+                <a href="#" className="ml-6 text-green-500">
+                  в наличии
+                </a>
+              </div>
+              <div className="flex items-end py-2">
+                <div className="text-[16px] font-bold">{item.price}</div>
+                <div className="flex text-[12px] mx-2 py-1 items-end">
+                  ₽<div className="px-1">/ шт</div>
+                </div>
+              </div>
+              <div className="flex">
+                <a
+                  href={item.url}
+                  className="bg-[#5661CB] text-[#fff] text-[14px] py-1 px-2 rounded-sm"
+                >
+                  Купить в 1 клик
+                </a>
+                <a
+                  href="#"
+                  className=" border-[#5661CB] text-[#5661CB] border rounded-sm py-1 px-3 mx-3"
+                >
+                  <i class="fa fa-check" aria-hidden="true"></i>
+                </a>
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
-  )
-}
+  );
+};
+export default Card;
--- src/pages/Home/MainContainer.jsx
+++ src/pages/Home/MainContainer.jsx
@@ -1,6 +1,112 @@
-import React from "react";
+import React, { useState } from "react";
+import Card from "./Card";
+import img from '../../assets/1.png'
+import capimg1 from '../../assets/Icons/capimg1.png'
+import capimg2 from '../../assets/Icons/capimg2.png'
+import capimg3 from '../../assets/Icons/capimg3.png'
 
 export default function MainContainer() {
+    const [prod, setProd] = useState([
+        {
+            id: 1,
+            title: 'Кирпич облицовочный M95 Воткинский',
+            img: img,
+            desc1: 'Кол-во на поддоне, шт. - 297',
+            desc2: 'Формат одинарный',
+            price: '4500',
+            cap: capimg3,
+            url: '#'
+        },
+        {
+            id: 2,
+            title: 'Кирпич облицовочный M95 Воткинский',
+            img: img,
+            desc1: 'Кол-во на поддоне, шт. - 297',
+            desc2: 'Формат одинарный',
+            price: '4500',
+            cap: capimg3,
+            url: '#'
+        },
+        {
+            id: 3,
+            title: 'Кирпич облицовочный M95 Воткинский',
+            img: img,
+            desc1: 'Кол-во на поддоне, шт. - 297',
+            desc2: 'Формат одинарный',
+            price: '4500',
+            cap: capimg3,
+            url: '#'
+        },
+        {
+            id: 4,
+            title: 'Кирпич облицовочный M95 Воткинский',
+            img: img,
+            desc1: 'Кол-во на поддоне, шт. - 297',
+            desc2: 'Формат одинарный',
+            price: '4500',
+            cap: capimg3,
+            url: '#'
+        },
+        {
+            id: 5,
+            title: 'Кирпич облицовочный M95 Воткинский',
+            img: img,
+            desc1: 'Кол-во на поддоне, шт. - 297',
+            desc2: 'Формат одинарный',
+            price: '4500',
+            cap: capimg3,
+            url: '#'
+        },
+        {
+            id: 6,
+            title: 'Кирпич облицовочный M95 Воткинский',
+            img: img,
+            desc1: 'Кол-во на поддоне, шт. - 297',
+            desc2: 'Формат одинарный',
+            price: '4500',
+            cap: capimg3,
+            url: '#'
+        },
+        {
+            id: 7,
+            title: 'Кирпич облицовочный M95 Воткинский',
+            img: img,
+            desc1: 'Кол-во на поддоне, шт. - 297',
+            desc2: 'Формат одинарный',
+            price: '4500',
+            cap: capimg2,
+            url: '#'
+        },
+        {
+            id: 8,
+            title: 'Кирпич облицовочный M95 Воткинский',
+            img: img,
+            desc1: 'Кол-во на поддоне, шт. - 297',
+            desc2: 'Формат одинарный',
+            price: '4500',
+            cap: capimg1,
+            url: '#'
+        },
+        {
+            id: 9,
+            title: 'Кирпич облицовочный M95 Воткинский',
+            img: img,
+            desc1: 'Кол-во на поддоне, шт. - 297',
+            desc2: 'Формат одинарный',
+            price: '4500',
+            url: '#'
+        },
+        {
+            id: 10,
+            title: 'Кирпич облицовочный M95 Воткинский',
+            img: img,
+            desc1: 'Кол-во на поддоне, шт. - 297',
+            desc2: 'Формат одинарный',
+            price: '4500',
+            cap: capimg3,
+            url: '#'
+        },
+    ])
   return (
     <div>
       <div className="flex justify-between items-center">
@@ -9,6 +115,9 @@ export default function MainContainer() {
           Перейти в раздел скидок
           <i class="fa fa-arrow-right mx-2" aria-hidden="true"></i>
         </a>
+      </div>
+      <div className="">
+        <Card prod={prod}/>
       </div>
     </div>
   );
