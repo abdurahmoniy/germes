8d1794eed80a4ed964eecc398257e2b22b95097b
abdurahmoniy <azizbekabdurahmoniy@gmail.com>
2023-08-01 16:35:40 +0500

    cart page responsived

--- frontend-master/src/pages/Cart/CartPage.jsx
+++ frontend-master/src/pages/Cart/CartPage.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { Link } from "react-router-dom";
 import TopBar from "../../components/TopBar";
-import { updateCartItem, removeFromCart } from './cartActions';
+import { updateCartItem, removeFromCart } from "./cartActions";
 
 const CartPage = () => {
   const dispatch = useDispatch();
@@ -15,11 +15,18 @@ const CartPage = () => {
   const handleIncrement = (index) => {
     dispatch(updateCartItem(index, cartItems[index].counterValue + 1));
   };
-  
+
   const handleDecrement = (index) => {
-    dispatch(updateCartItem(index, cartItems[index].counterValue > 0 ? cartItems[index].counterValue - 1 : 0));
+    dispatch(
+      updateCartItem(
+        index,
+        cartItems[index].counterValue > 0
+          ? cartItems[index].counterValue - 1
+          : 0
+      )
+    );
   };
-  
+
   const handleInputChange = (index, value) => {
     if (/^\d*$/.test(value)) {
       dispatch(updateCartItem(index, Number(value)));
@@ -41,55 +48,81 @@ const CartPage = () => {
           <div className="text-[20px]">Кирпичи</div>
           <div className="block py-4">
             {cartItems.map((item, index) => (
-              <div
-                key={index}
-                className="border flex w-[70%] rounded items-center py-8 my-4"
-              >
-                {/* Rendering image, title, and price */}
-                <div className="w-[15%] h-full">
-                  <div className="text-center flex justify-center">
+              <div className="border lg:border-none">
+                <div className="sm:hidden flex w-full py-6">
+                  <div className="text-center flex justify-center w-[50%]">
                     {item.img ? (
                       <img src={item.img} alt="" />
                     ) : (
                       <div>Image not available</div>
                     )}
                   </div>
-                </div>
-                <div className="w-[30%] h-full">
-                  <div className="text-[14px] font-semibold">{item.title}</div>
-                  <div className="text-[12px] py-1">
-                    {item.desc1} <br />
-                    {item.desc2}
-                  </div>
-                </div>
-                <div className="w-[20%] h-full">
-                  <div className="counter border flex px-2 w-[100px] rounded justify-between mx-auto">
-                    <div className="" onClick={() => handleDecrement(index)}>
-                      -
+                  <div className="">
+                    <div className="text-[14px] font-semibold">
+                      {item.title}
                     </div>
-                    <input
-                      value={item.counterValue}
-                      onChange={(e) => handleInputChange(index, e.target.value)}
-                      className="w-16 text-center"
-                    />
-                    <div className="" onClick={() => handleIncrement(index)}>
-                      +
+                    <div className="text-[12px] py-1">
+                      {item.desc1} <br />
+                      {item.desc2}
                     </div>
                   </div>
                 </div>
-                <div className="w-[20%] h-full flex justify-center">
-                  <div className="flex items-end py-2">
-                    <div className="text-[16px] font-bold">
-                      {calculateTotalPrice(item)}
-                    </div>
-                    <div className="flex text-[12px] mx-2 py-1 items-end">
-                      ₽
+                <div
+                  key={index}
+                  className="lg:border flex xl:w-[70%] w-full rounded items-center pb-4 sm:py-8 lg:my-4 px-12 justify-between"
+                >
+                  <div className="w-[15%] h-full hidden sm:block">
+                    <div className="text-center flex justify-center">
+                      {item.img ? (
+                        <img src={item.img} alt="" />
+                      ) : (
+                        <div>Image not available</div>
+                      )}
                     </div>
                   </div>
-                </div>
-                <div className="w-[15%] h-full flex justify-center">
-                  <i className="fa fa-star mx-3" aria-hidden="true"></i>
-                  <i className="fa-solid fa-trash" onClick={() => handleRemoveFromCart(index)}></i>
+                  <div className="sm:w-[30%] h-full hidden sm:block">
+                    <div className="text-[14px] font-semibold">
+                      {item.title}
+                    </div>
+                    <div className="text-[12px] py-1">
+                      {item.desc1} <br />
+                      {item.desc2}
+                    </div>
+                  </div>
+                  <div className="sm:w-[20%] h-full">
+                    <div className="counter border flex px-2 w-[100px] rounded justify-between mx-auto">
+                      <div className="" onClick={() => handleDecrement(index)}>
+                        -
+                      </div>
+                      <input
+                        value={item.counterValue}
+                        onChange={(e) =>
+                          handleInputChange(index, e.target.value)
+                        }
+                        className="w-16 text-center"
+                      />
+                      <div className="" onClick={() => handleIncrement(index)}>
+                        +
+                      </div>
+                    </div>
+                  </div>
+                  <div className="sm:-[20%] h-full flex justify-center">
+                    <div className="flex items-end py-2">
+                      <div className="text-[16px] font-bold">
+                        {calculateTotalPrice(item)}
+                      </div>
+                      <div className="flex text-[12px] mx-2 py-1 items-end">
+                        ₽
+                      </div>
+                    </div>
+                  </div>
+                  <div className="w-[20%] sm:w-[15%] h-full flex justify-center">
+                    <i className="fa fa-star mx-3" aria-hidden="true"></i>
+                    <i
+                      className="fa-solid fa-trash"
+                      onClick={() => handleRemoveFromCart(index)}
+                    ></i>
+                  </div>
                 </div>
               </div>
             ))}
